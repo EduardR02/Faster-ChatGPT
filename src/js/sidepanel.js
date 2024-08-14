97bbe9b4b0f28c44c9113302948c283dda258545
EduardE02 <eduard@rantsevich.net>
2024-08-14 20:26:08 +0200

    fake commit (added latest gpt-4o model)

--- src/js/sidepanel.js
+++ src/js/sidepanel.js
@@ -9,7 +9,7 @@ const MODELS = {
         "gpt-3.5-turbo": "gpt-3.5-turbo",
         "gpt-4": "gpt-4",
         "gpt-4-turbo": "gpt-4-turbo-preview",
-        "gpt-4o": "gpt-4o",
+        "gpt-4o": "chatgpt-4o-latest",
         "gpt-4o-mini": "gpt-4o-mini"
     },
     anthropic: {
